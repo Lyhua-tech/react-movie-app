1aa4632487a70314d09d3a3f78ec7695868490ac
Lyhua-tech <slyhour69@gmail.com>
2025-06-11 16:49:09 +0700

    change logo and name

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ const App = () => {
                 movies.length > 0 ? (
                   <>
                   <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-                    {movies.map((movie) => (
+                    {movies.filter((movie) => movie.adult === false).map((movie) => (
                       <MovieCard key={movie.id} movie={movie} />
                     ))}
                   </ul>
