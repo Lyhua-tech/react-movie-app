b0e68cb4e30db1d5db6438e7c2d9e9fdc2cdc571
Lyhua-tech <slyhour69@gmail.com>
2025-06-13 10:56:03 +0700

    reapply speed test

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { inject } from "@vercel/speed-insights/react";
-
-// Inject Vercel Speed Insights
-inject();
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
       <App />
+      <SpeedInsights />
     </StrictMode>
   </BrowserRouter>
 );
