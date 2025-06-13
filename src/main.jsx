51f8eebf62f1866ba7b7df37e56b5f77f37130c5
Lyhua-tech <slyhour69@gmail.com>
2025-06-13 10:53:36 +0700

    implement optimization test

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { inject } from "@vercel/speed-insights/react";
+
+// Inject Vercel Speed Insights
+inject();
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
